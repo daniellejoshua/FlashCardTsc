a0017bb15d3305bf0a16fbad60aa7382f0c3fb49
Danielle Joshua Esternon <daniellejoshua18@gmail.com>
2026-04-28 20:18:20 +0800

    ADD: login page and signup page, add some customHooks and login logic(still not good)

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(helmet());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(cookieParser());
 app.use("/api/user", userRoutes);
 app.use("/api/topic", topicRoutes);
