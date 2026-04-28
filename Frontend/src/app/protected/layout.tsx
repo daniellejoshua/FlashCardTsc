38ce57dad242fc3b73bb9c1d1b381ebce58a4014
Danielle Joshua Esternon <daniellejoshua18@gmail.com>
2026-04-29 00:57:02 +0800

    ADD: add toast and protected and public layouts for token checking

---
+++ Frontend/src/app/protected/layout.tsx
@@ -0,0 +1,13 @@
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
+export default async function ProtectedLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const cookieStore = cookies();
+  const token = (await cookieStore).get("accessToken")?.value;
+  if (!token) return redirect("/login");
+
+  return <>{children}</>;
+}
